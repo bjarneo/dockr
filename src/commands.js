f3d04f6280cb1d2b33bf03e443908180cad420d0
bjarne-overli <bjarne.overli@vg.no>
2016-04-11 22:14:47 +0200

    move the promise to spawn command

--- src/commands.js
+++ src/commands.js
@@ -1,29 +1,21 @@
 'use strict';
 
-const spawnCommand = require('./spawn');
+const spawn = require('./spawn');
 
 function build(tag, dockerFileLocation) {
-    return new Promise((resolve, reject) => {
-        spawnCommand('docker', ['build', '-t', tag, dockerFileLocation], exitCode => {
-            if (exitCode) {
-                reject('Docker build failed with exit code: ' + exitCode);
-            }
-
-            resolve(tag);
+    return spawn('docker', ['build', '-t', tag, dockerFileLocation])
+        .then(() => tag)
+        .catch(err => {
+            throw new Error(err);
         });
-    });
 }
 
 function push(tag) {
-    return new Promise((resolve, reject) => {
-        spawnCommand('docker', ['push', tag], exitCode => {
-            if (exitCode) {
-                reject('Docker push failed with exit code: ' + exitCode);
-            }
-
-            resolve(tag);
+    return spawn('docker', ['push', tag])
+        .then(() => tag)
+        .catch(err => {
+            throw new Error(err);
         });
-    });
 }
 
 module.exports = {
--- src/spawn.js
+++ src/spawn.js
@@ -2,14 +2,16 @@
 
 const spawn = require('child_process').spawn;
 
-module.exports = function spawnCommand(command, args, callback) {
+module.exports = function spawnCommand(command, args) {
     const run = spawn(command, args);
 
     run.stdout.on('data', data => console.log(data.toString('utf-8')));
 
     run.stderr.on('data', data => console.error(data.toString('utf-8')));
 
-    run.on('close', closeCode => callback(null, closeCode));
+    return new Promise((resolve, reject) => {
+        run.on('close', resolve);
 
-    run.on('exit', callback);
+        run.on('error', reject);
+    });
 };
