2b9b7f486fd87e89a7b9c39c4e5658a27aa2f28e
bjarneo <bjarneo@users.noreply.github.com>
2016-05-01 12:49:35 +0200

    reject if error occurs

--- src/spawn.js
+++ src/spawn.js
@@ -6,10 +6,10 @@ module.exports = function spawnCommand(command, args) {
     const run = spawn(command, args);
 
     run.stdout.on('data', data => console.log(data.toString('utf-8')));
-
-    run.stderr.on('data', data => console.error(data.toString('utf-8')));
-
+    
     return new Promise((resolve, reject) => {
+        run.stderr.on('data', data => reject(data.toString('utf-8')));
+    
         run.on('close', resolve);
 
         run.on('error', reject);
