9b11287bb72252b29b3ad527af9ee9eb93e616c9
bjarneo <bjarne.overli@vg.no>
2016-05-09 13:55:13 +0200

    adding spinner if verbose is false

--- src/spawn.js
+++ src/spawn.js
@@ -1,13 +1,18 @@
 'use strict';
 
 const spawn = require('child_process').spawn;
+const ora = require('ora');
 const config = require('./config');
+
+const spinner = ora('Creating and pushing docker image to registry');
 
 module.exports = function spawnCommand(command, args) {
     const run = spawn(command, args);
 
     if (!config.get('options').verbose) {
         console.log = () => {};
+
+        spinner.start();
     }
 
     run.stdout.on('data', data => console.log(data.toString('utf-8')));
@@ -15,7 +20,11 @@ module.exports = function spawnCommand(command, args) {
     return new Promise((resolve, reject) => {
         run.stderr.on('data', data => reject(data.toString('utf-8')));
 
-        run.on('close', resolve);
+        run.on('close', () => {
+            spinner.stop();
+
+            resolve();
+        });
 
         run.on('error', reject);
     });
